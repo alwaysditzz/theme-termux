a4afa64c04c769eca9ea5c435527bdc4c3653a31
alwaysditzz <ditzzff74@gmail.com>
2025-09-03 16:22:25 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-green="\033[1;32m"
-red="\033[1;31m"
-reset="\033[0m"
+# Warna
+green="\033[1;32m"; red="\033[1;31m"; cyan="\033[1;36m"; reset="\033[0m"
 
 while true; do
     clear
-    echo -e "${red}"
+    echo -e "${cyan}"
     figlet "TOOLS-TERMUX" | lolcat
     echo -e "${reset}"
     echo -e "${green}1) Install Theme"
     echo -e "2) Uninstall Theme"
     echo -e "3) Exit${reset}"
     echo ""
-    read -p "Pilih opsi (1/2/3): " pilihan
+    read -p "Pilih opsi (1/2/3): " opsi
 
-    case $pilihan in
+    case $opsi in
         1)
             bash install.sh
             read -p "Tekan [Enter] untuk kembali ke menu..."
@@ -25,7 +24,7 @@ while true; do
             read -p "Tekan [Enter] untuk kembali ke menu..."
             ;;
         3)
-            echo -e "${red}Bye! Keluar dari menu.${reset}"
+            echo -e "${cyan}Bye! Keluar dari menu.${reset}"
             exit 0
             ;;
         *)
